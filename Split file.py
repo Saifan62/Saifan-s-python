d88bd783ba674aa8d3fdff0c95b7bcdccc7f6890
Saifan62 <saifanzaman681@gmail.com>
2025-07-26 10:11:27 +0600

    Add initial implementation of file reading and word splitting in Split file.py

---
+++ Split file.py
@@ -0,0 +1,7 @@
+with open('Codingal.txt', 'r') as file:
+    data = file.readlines()
+    print("Words in this file are....")
+    for line in data:
+        word= line.split()
+        print(word)
+file.close()
